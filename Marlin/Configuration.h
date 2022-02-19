7415ef9caf9cc1b7105b361b43dd00ea963bc835
ZDH <z.denizhasirci95@gmail.com>
2022-02-19 10:32:20 +0300

    Probe adjustment and bed leveling type.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1179,7 +1179,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -15, 53, -1.95 }
+#define NOZZLE_TO_PROBE_OFFSET { -16, 45, -1.85 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1543,8 +1543,8 @@
  *   With an LCD controller the process is guided step-by-step.
  */
 //#define AUTO_BED_LEVELING_3POINT
-#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_BILINEAR
+//#define AUTO_BED_LEVELING_LINEAR
+#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
